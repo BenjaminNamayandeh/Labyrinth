f2f5270b82484cd032f20f644ea12ff7beea5c14
AS1003008 <99190579+AS1003008@users.noreply.github.com>
2022-12-06 14:21:26 -0500

    Added even more borders

--- My project/Assets/Scripts/PickUp.cs
+++ My project/Assets/Scripts/PickUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PickUp : MonoBehaviour
+public class PickUp : MonoBehaviour // This class enables the player to pick up wood in the maze
 {
     [SerializeField] private Slider slider;
     [SerializeField] private float speed;
@@ -14,21 +14,21 @@ public class PickUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Wood")
+        if (collision.tag == "Wood") // If player hits wood, add wood count and increase wood bar value
         {
             woodCount += 1;
             var wood = collision.gameObject;
-            Destroy(wood);
+            Destroy(wood); // Wood is destroyed
             slider.value += 0.3f;
         }
 
-        if (collision.tag == "Door")
+        if (collision.tag == "Door") // If door reached, the round ends
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
-    private void Update()
+    private void Update() // Slider decreases with time
     {
         slider.value -= Time.deltaTime * speed;
     }
