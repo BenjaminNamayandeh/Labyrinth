e1e25c423d33e444bb34b223c84441ae4b82ac7c
BenjaminNamayandeh <113051650+BenjaminNamayandeh@users.noreply.github.com>
2022-12-14 19:02:43 -0500

    Player animations
    Changes:
    - added player animations

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 moveDirection;
+    private Animator animator; 
 
     public float speed;
 
@@ -14,12 +15,21 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = (transform.up * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
     }
 
     private void FixedUpdate()
